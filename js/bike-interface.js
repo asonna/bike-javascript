d87fcdd7d59fdde82ec4bbc02e65f60bda75d8a6
Austin Blain and Annie Sonna <austin@>
2017-01-05 11:49:16 -0800

    add more scss effects and correct image to be able to refresh prior to posting new one

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -2,6 +2,7 @@ var Bike = require('./../js/bike.js').bikeModule;
 
 var displayManufacturer = function(serial, manufacturerData, image) {
   $('.showManufacturer').text(serial + " matches bike from " + manufacturerData + " manufacturer.");
+  $('.showImg').empty();
   $('.showImg').append('<img src="' +image + '">');
 };
 
