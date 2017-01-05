975a2ca8d9958ac6bbefa1f313dc43874255abd1
Annie Sonna and Austin Blain <annie+austin@>
2017-01-05 15:46:02 -0800

    add new prototype to allow query of list of bikes by location and manufacturer

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,17 +1,35 @@
 var Bike = require('./../js/bike.js').bikeModule;
 
 var displayManufacturer = function(serial, manufacturerData, image) {
-  $('.showManufacturer').text(serial + " matches bike from " + manufacturerData + " manufacturer.");
+  $('.showManufacturer').text(serial + " manufactured bike stolen in this area " + manufacturerData);
   $('.showImg').empty();
-  $('.showImg').append('<img src="' +image + '">');
+  $('.showImg').append('<img src="' + image + '">');
+};
+
+var displayBikes = function(location, manufacturer, bikeArray) {
+  var i = '';
+  // console.log(bikeArray);
+  for(i=0; i< bikeArray.length; ++i) {
+    $('.showBikes').append('<img src="' + bikeArray[i].large_img + '">');
+    // console.log(bikeArray[i]);
+  }
 };
 
 $(document).ready(function() {
   var currentBikeObject = new Bike();
   $('.bikeSerialForm').submit(function(event) {
+    // debugger;
     event.preventDefault();
     var serial = $('#serial').val();
     $('#serial').val();
     currentBikeObject.getManufacturer(serial, displayManufacturer);
+  });
+  $('.byLocationForm').submit(function(event) {
+    event.preventDefault();
+    var location = $('#locationInput').val();
+    var manufacturer = $("#manufacturerInput").val();
+    $("#locationInput").val();
+    $("#manufacturerInput").val();
+    currentBikeObject.getList(location, manufacturer, displayBikes);
   });
 });
--- js/bike.js
+++ js/bike.js
@@ -4,12 +4,22 @@ function Bike() {
 }
 
 Bike.prototype.getManufacturer = function(serial, displayFunction){
+  // debugger;
   $.get('https://bikeindex.org:443/api/v3/search?serial=' + serial).then(function(response) {
     displayFunction(serial, response.bikes[0].manufacturer_name, response.bikes[0].large_img);
-    console.log(response.bikes);
   }).fail(function(error) {
     $('.showManufacturer').text(error.responseJSON.message);
     $('.showImg').append(error.responseJSON.message);
+  });
+};
+
+Bike.prototype.getList = function(location, manufacturer, displayFunction) {
+  // debugger;
+  $.get('https://bikeindex.org:443/api/v3/search?manufacturer=' + manufacturer + '&location=' + location + '&stolenness=proximity').then(function(response) {
+    displayFunction(location, manufacturer, response.bikes);
+    // console.log(response.bikes);
+  }).fail(function(error) {
+    $('.showBikes').text(error.responseJSON.message);
   });
 };
 
